62e89e17e8f4577fe93c6b619c524313bfbbdd7e
Harri Morgan <114748904+s4205066@users.noreply.github.com>
2024-03-14 11:17:53 +0000

    Update 2_langchain_chatbot.py

--- pages/2_langchain_chatbot.py
+++ pages/2_langchain_chatbot.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import random
+from langchain_openai import OpenAI
 import time
 
 st.title("Langchain Chatbot")
+
+api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+llm = OpenAI(openai_api_key = api_key)
+
+def response_generator(message):
+    response = llm.invoke(message)
+
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+with st.chat_message("assistant"):
+    st.write_stream("I'm an AI Assistant. How can i help?")
 
 if "langchain_messages" not in st.session_state:
     st.session_state.langchain_messages = []
@@ -17,22 +31,13 @@ if prompt := st.chat_input("Enter Message:"):
 
     st.session_state.langchain_messages.append({"role": "user", "content": prompt})
 
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
 
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+    st.session_state.langchain_messages.append({"role": "assistant", "content": response})
 
-with st.chat_message("assistant"):
-    response = st.write_stream(response_generator())
 
-st.session_state.langchain_messages.append({"role": "assistant", "content": response})
+
+
 
 
