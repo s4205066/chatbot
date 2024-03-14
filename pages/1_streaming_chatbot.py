3f99725856e16684f063beaaf183ffcea5b75e4b
Harri Morgan <114748904+s4205066@users.noreply.github.com>
2024-03-14 11:07:34 +0000

    fix

--- pages/1_streaming_chatbot.py
+++ pages/1_streaming_chatbot.py
@@ -7,7 +7,7 @@ st.title("Streaming Chatbot")
 if "messages" not in st.session_state:
     st.session_state.stream_messages = []
 
-for message in st.session_state.messages:
+for message in st.session_state.stream_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
--- pages/2_langchain_chatbot.py
+++ pages/2_langchain_chatbot.py
@@ -7,7 +7,7 @@ st.title("Langchain Chatbot")
 if "messages" not in st.session_state:
     st.session_state.langchain_messages = []
 
-for message in st.session_state.messages:
+for message in st.session_state.langchain_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
