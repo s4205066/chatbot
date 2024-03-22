00903e617a5cdef2fcc35c896612615f0d3b4891
Harri Morgan <114748904+s4205066@users.noreply.github.com>
2024-03-22 10:21:17 +0000

    commit

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,27 +1,46 @@
 import streamlit as st
-import ollama
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
 
-st.title("S4205066 Chatbot")
+# https://discuss.streamlit.io/t/adding-a-long-pdf-as-a-custom-data-source/57348
+# https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can i help?"}]
+
+openai.api_key = st.secrets.openai_key
+st.header("S4205066 Chatbot")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question!"}
+    ]
+
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading data."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the University of Gloucestershire and your job is to answer questions. Assume that all questions are related to the University of Gloucestershire. Keep your answers professional and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+    
+index = load_data()
+
+chat_engine = index.as_chat_engine(chat_mode="condese_question", verbose=True)
+
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages:
-    if message["role"] == "user":
-        st.chat_message(message["role"], avatar="😀").write(message["content"])
-    else:
-        st.chat_message(message["role"], avatar="💬").write(message["content"])
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-def generate_response():
-    response = ollama.chat(model="talisman825/unichatbot", stream=True, messages=st.session_state.messages)
-    for partialResponse in response:
-        token = partialResponse["message"]["content"]
-        st.session_state["full_message"] += token
-        yield token
-
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user", avatar="😀").write(prompt)
-    st.session_state["full_message"] = ""
-    st.chat_message("assistant", avatar="💬").write_stream(generate_response)
-    st.session_state.messages.append({"role": "assistant", "content": st.session_state["full_message"]})
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message= {"role": "assistant", "content": response}
+            st.session_state.messages.append(message)
